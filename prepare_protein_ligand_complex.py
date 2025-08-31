ea394713fdc3c7357403ea171bc12fdc95277117
Akash Pandya <86837869+Kash-Pandz@users.noreply.github.com>
2025-08-31 13:23:45 +0100

    Update prepare_protein_ligand_complex.py

--- prepare_protein_ligand_complex.py
+++ prepare_protein_ligand_complex.py
@@ -9,6 +9,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Prepare protein-ligand system.")
     parser.add_argument("--input_pdb", required=True, help="Input protein-ligand complex")
     parser.add_argument("--ph", type=float, default=7.0, help="pH for protein")
+    parser.add_argument("--ligand_resname", default="LIG", help="Ligand resname")
     parser.add_argument("--ligand_charge", type=int, default=0, help="Net charge of ligand")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose logging")
     return parser.parse_args()
@@ -50,32 +51,38 @@ def prepare_protein(input_pdb: str, output_pdb: str = "prot_p4a.pdb") -> str:
     return output_pdb
 
 
-def extract_ligand(complex_pdb: str, output_pdb: str = "LIG_final.pdb", resid: str = "LIG") -> str:
-    logger.info(f"Extracting ligand {resid} from complex...")
-    run_cmd(f"grep '^HETATM' {complex_pdb} | grep ' {resid} ' > {output_pdb}", shell=True)
+def extract_ligand(complex_pdb: str, output_pdb: str = "LIG_final.pdb", resn: str = "LIG") -> str:
+    logger.info(f"Extracting ligand {resn} from complex...")
+    run_cmd(f"grep '^HETATM' {complex_pdb} | awk '{{if ($4==\"{resn}\") print $0}}' > {output_pdb}", shell=True)
     return output_pdb
 
 
-def prepare_ligand(ligand_pdb: str, charge: int = 0):
+def prepare_ligand(ligand_pdb: str, charge: int = 0, resn: str = "LIG"):
     logger.info("Protonating ligand...")
-    run_cmd(["obabel", "-ipdb", ligand_pdb, "-opdb", "-O", "LIG_H.pdb", "-h"])
+    h_pdb = f"{resid}_H.pdb
+    run_cmd(["obabel", "-ipdb", ligand_pdb, "-opdb", "-O", "h_pdb", "-h"])
 
     logger.info("Running antechamber (SYBYL cleanup)...")
+    sybl_mol2 = f"{resid}_sybl.mol2"
     run_cmd([ 
-        "antechamber", "-i", "LIG_final_H.pdb", "-fi", "pdb", "-o", "LIG_sybl.mol2",
+        "antechamber", "-i", h_pdb, "-fi", "pdb", "-o", sybl_mol2,
         "-fo", "mol2", "-j", "5", "-at", "sybyl", "-dr", "no", "-s", "2"
-        ])
+    ])
     
     logger.info("Running antechamber (GAFF2 + charges)...")
+    final_mol2 = f"{resid}.mol2"
     run_cmd([
-        "antechamber", "-i", "LIG_sybl.mol2", "-fi", "mol2", "-o", "LIG.mol2", "-fo", "mol2",
-        "-at", "gaff2", "-c", "bcc", "-s", "2", "-rn", "LIG", "-nc", str(charge)
-        ])
+        "antechamber", "-i", sybl_mol2, "-fi", "mol2", "-o", final.mol2, "-fo", "mol2",
+        "-at", "gaff2", "-c", "bcc", "-s", "2", "-rn", resn, "-nc", str(charge)
+    ])
     
     logger.info("Running parmchk2...")
-    run_cmd(["parmchk2", "-i", "LIG.mol2", "-f", "mol2", "-o", "LIG.frcmod"])
-
-    return "LIG.mol2", "LIG.frcmod"
+    frcmod = f"{resid}.frcmod"
+    run_cmd([
+        "parmchk2", "-i", final.mol2, "-f", "mol2", "-o", frcmod
+    ])
+    
+    return final_mol2, frcmod
 
 
 def write_tleap(protein_pdb: str, lig_mol2: str, lig_frcmod: str, output: str = "tleap.in") -> str:
@@ -111,15 +118,15 @@ def main():
     args = parse_args()
     configure_logging(args.verbose)
 
-    complex_file = str(args.complex.resolve())
+    complex_file = str(args.input_pdb.resolve())
     
     logger.info("Step 1: Protein preparation...")
     propka_pdb = protonate_protein(complex_file, pH=args.ph)
     protein_pdb = prepare_protein(propka_pdb)
     
     logger.info("Step 2: Ligand preparation...")
-    ligand_pdb = extract_ligand(complex_file)
-    lig_mol2, lig_frcmod = prepare_ligand(ligand_pdb, charge=args.charge)
+    ligand_pdb = extract_ligand(complex_file, resn=args.ligand_resname)
+    lig_mol2, lig_frcmod = prepare_ligand(ligand_pdb, charge=args.ligand_charge)
 
     logger.info("Step 3: Building system with tleap...")
     tleap_in = write_tleap(protein_pdb, lig_mol2, lig_frcmod)
