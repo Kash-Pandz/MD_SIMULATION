0ddc033fdbf572ebfdec127c602fd972705f3f04
Akash Pandya <86837869+Kash-Pandz@users.noreply.github.com>
2025-09-01 11:03:13 +0100

    Update gmx_md_job.sh

--- gmx_md_job.sh
+++ gmx_md_job.sh
@@ -20,6 +20,10 @@ GMX="srun gmx_mpi"
 echo "Job started on $(date)"
 echo "Running on $SLURM_NNODES node(s), $SLURM_NTASKS task(s), $SLURM_CPUS_PER_TASK CPU threads, $SLURM_GPUS GPU(s)"
 
+# Energy minimisation
+gmx grompp -f em.mdp -c SYSTEM.gro -p SYSTEM.top -o em.tpr
+$GMX mdrun -deffnm em -ntmpi 1 -ntomp $SLURM_CPUS_PER_TASK -pin on -gpu_id 0
+
 # NVT equilibration 
 gmx grompp -f nvt.mdp -c SYSTEM_EM.gro -r SYSTEM_EM.gro -p SYSTEM.top -o nvt.tpr
 $GMX mdrun -deffnm nvt -ntmpi 1 -ntomp $SLURM_CPUS_PER_TASK -pin on -gpu_id 0
